4055dfd21bc6ff4b4d173d6c6b1ab7b6761025fb
Anahi Quintero <anitaquintero99@gmail.com>
2021-07-15 20:46:41 -0500

    methods to java

--- Java/Main.java
+++ Java/Main.java
@@ -1,5 +1,19 @@
+import javax.swing.text.View;
+
 public class Main {
-  public static void main(String[] args){
+  public static void main(String[] args) {
     System.out.println("Hello world");
-  }//End of main
-}//End of FirstJavaProgram Class
+    Car car = new Car();
+    car.license = "AMQ123";
+    car.driver = "Andres Herrera";
+    car.passenger = 4;
+    car.printDataCar();
+
+    Car car2 = new Car();
+    car2.license = "Qwe123";
+    car2.driver = "Anahi Quintero";
+    car2.passenger = 3;
+    car2.printDataCar();
+
+  }// End of main
+}// End of FirstJavaProgram Class
