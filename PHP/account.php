648334c7c3bfc94347b4eedc5fee4c513e271789
Anahi Quintero <anitaquintero99@gmail.com>
2021-08-11 21:48:35 -0500

    new changes to java, Add new values

--- PHP/account.php
+++ PHP/account.php
@@ -6,4 +6,9 @@ class Account {
   public $document = string;
   public $email = string;
   public $password = string;
+
+  public function__constrcut($name, $document){
+    $this->name = $name;
+    $this->document = $document;
+  }
 }
